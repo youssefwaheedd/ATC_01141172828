1a5a2df59e0cae0f2270242efe640751a51e1825
youssefwaheed123 <youssefwaheed@gmail.com>
2025-05-08 20:35:17 +0300

    fix theme toggle

--- frontend/src/components/Theme.tsx
+++ frontend/src/components/Theme.tsx
@@ -14,8 +14,8 @@ export function Theme() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button size="icon" className="cursor-pointer bg-transparent">
+      <DropdownMenuTrigger>
+        <Button size="icon" className="cursor-pointer bg-transparent ">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-black" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:hover:text-black dark:text-white " />
         </Button>
