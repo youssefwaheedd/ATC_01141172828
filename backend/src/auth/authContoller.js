d35a9f30132d9f7487066ccbc88ed6ce3bf5cbd6
youssefwaheed123 <youssefwaheed@gmail.com>
2025-05-08 20:24:49 +0300

    add rewrite rules to vercel.json for SPA routing

--- backend/src/auth/authContoller.js
+++ backend/src/auth/authContoller.js
@@ -27,7 +27,7 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
